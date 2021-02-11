2c323c4c7ae8f86643b4454348e6a8bc1b2ad7a8
olus <oleksy.marcin@gmail.com>
2021-02-11 02:05:32 +0100

    Fix add ingredient to step

--- resources/views/livewire/recipe-step-edition.blade.php
+++ resources/views/livewire/recipe-step-edition.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="md:flex md:space-x-2 w-full" x-data="{ show: true }" x-show="show" @mouseenter="showIngredients($el)" @mouseleave="hideIngredients($el)">
         <div class="md:w-10/12">
-            <x-textarea wire:model.lazy="step.description" x-ref="description" class="block mt-1 w-full" :placeholder="__('Step description')">{{ $step->description }}</x-textarea>
+            <x-textarea wire:model.defer="step.description" x-ref="description" class="block mt-1 w-full" :placeholder="__('Step description')">{{ $step->description }}</x-textarea>
 
             <x-validation-error class="mb-4" :errors="$errors->get('step.description')" />
 
